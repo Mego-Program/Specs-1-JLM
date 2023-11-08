242ff99b1b28c48c78545059bf6a4c2293b21482
itsik729 <ik729212@gmail.com>
2023-11-08 15:39:40 +0200

    test

--- src/component/specs/AddNewSpecs.jsx
+++ src/component/specs/AddNewSpecs.jsx
@@ -1,77 +1,11 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
+// // import { useState } from "react";
+// // import { NavLink } from "react-router-dom";
 
-export default function MyForm() {
-  const [inputs, setInputs] = useState({
-    input1: "",
-    input2: "",
-    input3: "",
-    input4: "",
-    input5: "",
-    input6: "",
-  });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+// //       </div>
+// //       <NavLink to="/">Add New Specs </NavLink>
+// //       <button> next page </button>
+// //     </div>
+// //   );
+// }
 
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          name="input1"
-          value={inputs.input1}
-          onChange={handleInputChange}
-          placeholder="כאן התווית הרלוונטית ל-input 1"
-        />
-        <input
-          type="text"
-          name="input2"
-          value={inputs.input2}
-          onChange={handleInputChange}
-          placeholder="כאן התווית הרלוונטית ל-input 2"
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          name="input3"
-          value={inputs.input3}
-          onChange={handleInputChange}
-          placeholder="כאן התווית הרלוונטית ל-input 3"
-        />
-
-        <input
-          type="text"
-          name="input4"
-          value={inputs.input4}
-          onChange={handleInputChange}
-          placeholder="כאן התווית הרלוונטית ל-input 4"
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          name="input5"
-          value={inputs.input5}
-          onChange={handleInputChange}
-          placeholder="כאן התווית הרלוונטית ל-input 5"
-        />
-        <input
-          type="text"
-          name="input6"
-          value={inputs.input6}
-          onChange={handleInputChange}
-          placeholder="use"
-        />
-      </div>
-      <NavLink to="/">Add New Specs </NavLink>
-      <button> next page </button>
-    </div>
-  );
-}
