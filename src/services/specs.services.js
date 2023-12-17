8f9cdeddf7677a50a79d76744be4c1f27a7c9b00
Simcha Sucot <simchasucot@gmail.com>
2023-12-17 16:07:34 +0200

    update the server URL

--- src/services/specs.services.js
+++ src/services/specs.services.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 axios.defaults.baseURL = "https://specs-1-jlm-server-tzvis-projects.vercel.app/specs";
 const getAllSpecs = async () => {
   try {
     const req = await axios.get("/");
-=======
-const getAllSpecs = async () => {
-  try {
-    const req = await axios.get("http://localhost:4000/specs/");
->>>>>>> ab1a6983abcbad074c47feada48992a2ed41686f
     return req.data.specs;
   } catch (error) {
     console.error(error);
@@ -19,11 +13,7 @@ const getAllSpecs = async () => {
 
 const getSpecs = async (id) => {
   try {
-<<<<<<< HEAD
     const req = await axios.get(`/${id}`);
-=======
-    const req = await axios.get(`http://localhost:4000/specs/${id}`);
->>>>>>> ab1a6983abcbad074c47feada48992a2ed41686f
     return req.data.specs;
   } catch (error) {
     console.error(error);
@@ -33,11 +23,7 @@ const getSpecs = async (id) => {
 
 const addSpecs = async (specs) => {
   try {
-<<<<<<< HEAD
     const req = await axios.post("/createspecs", specs);
-=======
-    const req = await axios.post("http://localhost:4000/specs/createspecs", specs);
->>>>>>> ab1a6983abcbad074c47feada48992a2ed41686f
     return req.data.message;
   } catch (error) {
     return;
@@ -46,11 +32,7 @@ const addSpecs = async (specs) => {
 
 const deletedSpecs = async (id) => {
   try {
-<<<<<<< HEAD
     const req = await axios.delete(`/${id}`);
-=======
-    const req = await axios.delete(`http://localhost:4000/specs/${id}`);
->>>>>>> ab1a6983abcbad074c47feada48992a2ed41686f
     return req.data.message;
   } catch (error) {
     return;
@@ -59,11 +41,7 @@ const deletedSpecs = async (id) => {
 const editSpecs = async (id, specs) => {
   console.log(specs);
   try {
-<<<<<<< HEAD
     const req = await axios.patch(`/${id}`, specs);
-=======
-    const req = await axios.patch(`http://localhost:4000/specs/${id}`, specs);
->>>>>>> ab1a6983abcbad074c47feada48992a2ed41686f
     return req.data.message;
   } catch (error) {
     return;
